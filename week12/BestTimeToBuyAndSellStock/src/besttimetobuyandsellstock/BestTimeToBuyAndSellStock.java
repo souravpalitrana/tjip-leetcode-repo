b7eba2b56039b20c0d1e29c2db4cb2411944f4b5
Sourav Palit <souravpalitrana@gmail.com>
2022-08-15 23:35:35 +0600

    Add the solution of the problem best time to buy and sell stock

---
+++ week12/BestTimeToBuyAndSellStock/src/besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package besttimetobuyandsellstock;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class BestTimeToBuyAndSellStock {
+
+    // TC: O(n) SC: O(1)
+     public int maxProfit(int[] prices) {
+        // as we could not sell without buying it so we can start checking from
+        // the second day
+        int prevMinPrice = prices[0];
+        int maxProfit = 0;
+        for (int dayIdx = 1; dayIdx < prices.length; dayIdx++) {
+            if (prices[dayIdx] > prevMinPrice) {
+                maxProfit = Math.max(maxProfit, prices[dayIdx] - prevMinPrice);
+            } else {
+                prevMinPrice = prices[dayIdx]; 
+            }
+        }
+        
+        return maxProfit;
+    }
+    
+}
